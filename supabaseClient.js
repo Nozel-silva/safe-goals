f2c36eb0f116c8ac89e91d69cc462085ca5dcfa3
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2025-07-04 11:59:30 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://your-project.supabase.co'; // replace this
-const SUPABASE_ANON_KEY = 'your-anon-key'; // replace this
+const SUPABASE_URL = ' https://supabase.com/dashboard/project/ccmsjcnuyrngqxwrswfe'; // replace this
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjbXNqY251eXJuZ3F4d3Jzd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MjU2MjgsImV4cCI6MjA2NzIwMTYyOH0.dkjCo2bgDMf923VKESkyMLsULo7IhmsYb6r-4Dn6SRY'; // replace this
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
