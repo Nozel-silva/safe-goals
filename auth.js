5ef8dbb3aefa1dee4e062d2f5c953c278e41e425
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2026-05-09 18:50:47 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+const { createClient } = window.supabase
 const supabase = window.supabase.createClient(
   'https://ccmsjcnuyrngqxwrswfe.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjbXNqY251eXJuZ3F4d3Jzd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MjU2MjgsImV4cCI6MjA2NzIwMTYyOH0.dkjCo2bgDMf923VKESkyMLsULo7IhmsYb6r-4Dn6SRY'
